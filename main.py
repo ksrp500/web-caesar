d05337c10e7133c9ef5dca0e54ae9f2cf2525b51
Radhakrishnan <srkpnj@gmail.com>
2018-10-17 21:24:58 -0500

    Added a title and some info

--- main.py
+++ main.py
@@ -11,23 +11,45 @@ form = """
     <head>
         <style>
             form {{
-                background-color: #eee;
+                background-color: #C1E7EA;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
-                font: 16px arial black;
+                font: 20px arial black;
                 border-radius: 10px;
+
             }}
             textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
             }}
+            h1 {{
+                    padding: 10vh 0 0 0;
+                    font-family: arial;
+                    text-align: center;
+                    color: #FA8072;
+            }}
+            p {{
+                    font-family: verdana;
+                    text-align: center;
+                    color: blue;
+            }}
+            input[type=submit] {{
+                    background-color: magenta;
+                    border: double;
+                    padding: 10px 22px;
+            }}
         </style>
     </head>
     <body>
+
       <!-- create your form here -->
-       <form method="POST">
+        <h1>Web Caesar </h1>
+        <p>Enter a script and the number of letters to rotate the message.</p>
+        <p>Test: Rotate by 13 and agian by 13 will return the original.</p>
+        
+        <form method="POST">
             <label for="rot">Rotate by:</label>
             <input type="text" name="rot" value="0" />
             <br />
